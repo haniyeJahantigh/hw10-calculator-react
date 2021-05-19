b7931ff9590ee7c5c5392be6ca1f32d00d22de53
haniyeJahantig <safehuman30@gmail.com>
2021-05-19 11:47:49 +0430

    all problem with calculator done

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ const App = () => {
   };
 
   const calculate = () => {
-    // try {
-    // setResult(eval(result).tostring());
-    // } catch (err) {
-    //   setResult("Error");
-    // }
+    try {
+      setResult(eval(result).toString());
+    } catch (err) {
+      setResult("Error");
+    }
   };
 
   return (
@@ -30,79 +30,10 @@ const App = () => {
       <Result result={result} />
       <Keypad
         handleClick={handleClick}
-
-        // calculate();
+        clear={clear}
+        backspace={backspace}
+        calculate={calculate}
       />
-      {/* <div className="button">
-        <button name="CE" onClick={clear} id="clear" className="colorebtn">
-          Clear
-        </button>
-        <button
-          name="C"
-          onClick={backspace}
-          id="backspace"
-          className="colorebtn"
-        >
-          C
-        </button>
-        <button name="/" onClick={onClick} className="colorebtn">
-          &divide;
-        </button>
-        <br />
-
-        <button name="7" onClick={onClick}>
-          7
-        </button>
-        <button name="8" onClick={onClick}>
-          8
-        </button>
-        <button name="9" onClick={onClick}>
-          9
-        </button>
-        <button name="*" onClick={onClick} className="colorebtn">
-          &times;
-        </button>
-        <br />
-
-        <button name="4" onClick={onClick}>
-          4
-        </button>
-        <button name="5" onClick={onClick}>
-          5
-        </button>
-        <button name="6" onClick={onClick}>
-          6
-        </button>
-        <button name="-" onClick={onClick} className="colorebtn">
-          &ndash;
-        </button>
-        <br />
-
-        <button name="1" onClick={onClick}>
-          1
-        </button>
-        <button name="2" onClick={onClick}>
-          2
-        </button>
-        <button name="3" onClick={onClick}>
-          3
-        </button>
-        <button name="+" onClick={onClick} className="colorebtn">
-          +
-        </button>
-
-        <button name="0" onClick={onClick}>
-          0
-        </button>
-        <button name="." onClick={onClick}>
-          .
-        </button>
-        <button name="=" onClick={calculate} id="equal" className="colorebtn">
-          =
-        </button>
-      </div>
-
-      */}
     </div>
   );
 };
--- src/componenets/Keypad.jsx
+++ src/componenets/Keypad.jsx
@@ -5,27 +5,27 @@ const Keypad = ({handleClick , clear,backspace,calculate}) => {
        
             <div className="button">
                 <button name="CE" onClick={clear} id="clear"  className="colorebtn">Clear</button>
-                <button name="C" onClick={()=>backspace} id="backspace" className="colorebtn">C</button>
-                <button name="/" onClick={(e)=>handleClick()} className="colorebtn">&divide;</button><br/>
+                <button name="C" onClick={backspace} id="backspace" className="colorebtn">C</button>
+                <button name="/" onClick={handleClick} className="colorebtn">&divide;</button><br/>
 
                 <button name="7" onClick={handleClick}>7</button>
-                <button name="8" onClick={()=>handleClick}>8</button>
-                <button name="9" onClick={()=>handleClick}>9</button>
-                <button name="*" onClick={()=>handleClick} className="colorebtn">&times;</button><br/>
+                <button name="8" onClick={handleClick}>8</button>
+                <button name="9" onClick={handleClick}>9</button>
+                <button name="*" onClick={handleClick} className="colorebtn">&times;</button><br/>
 
-                <button name="4" onClick={()=>handleClick}>4</button>
-                <button name="5" onClick={()=>handleClick}>5</button>
-                <button name="6" onClick={()=>handleClick}>6</button>
-                <button name="-" onClick={()=>handleClick} className="colorebtn">&ndash;</button><br/>
+                <button name="4" onClick={handleClick}>4</button>
+                <button name="5" onClick={handleClick}>5</button>
+                <button name="6" onClick={handleClick}>6</button>
+                <button name="-" onClick={handleClick} className="colorebtn">&ndash;</button><br/>
 
-                <button name="1" onClick={()=>handleClick}>1</button>
-                <button name="2" onClick={()=>handleClick}>2</button>
-                <button name="3" onClick={()=>handleClick}>3</button>
-                {/* <button name="+" onClick={()=>onClick} className="colorebtn">+</button>
+                <button name="1" onClick={handleClick}>1</button>
+                <button name="2" onClick={handleClick}>2</button>
+                <button name="3" onClick={handleClick}>3</button>
+                <button name="+" onClick={handleClick} className="colorebtn">+</button>
 
-                <button name="0" onClick={()=>onClick}>0</button>
-                <button name="." onClick={()=>onClick}>.</button>
-                <button name="=" onClick={()=>calculate} id="equal" className="colorebtn">=</button> */}
+                <button name="0" onClick={handleClick}>0</button>
+                <button name="." onClick={handleClick}>.</button>
+                <button name="=" onClick={calculate} id="equal" className="colorebtn">=</button>
             </div>
        
     )
